ea8c0a0e8d609078fbd068d20a400abd538dc754
Samuel Arao <samuel.arao@gmail.com>
2026-07-15 07:45:17 -0300

    adicionar identificacao usuarios no projeto de chat-app

--- projects/chat-app/client.js
+++ projects/chat-app/client.js
@@ -1,17 +1,64 @@
 const net = require("net")
+const readline = require("readline/promises")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const clearLine = (dir) => {
+    return new Promise((resolve, rejects) =>{
+        process.stdout.clearLine(dir, () => {
+            resolve();
+        });
+    })
+}
+
+
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, rejects) => {
+        process.stdout.moveCursor(dx, dy, ()=>{
+            resolve();
+        })
+    })
+}
+
+let id;
 
 const client = net.createConnection({
     host: "localhost",
     port: "3008"
 },
-() => {
+async () => {
     console.log("Connected to the server!");
+
+    const ask = async () => {
+        const message = await rl.question("Enter a message > ")
+        await moveCursor(0,-1);
+        await clearLine(0);
+        client.write(`User ${id}: ${message}`)
+    }
+
+
+    client.on("data", async (data)=> {
+        if(data.toString('utf-8').substring(0,2) === "id"){
+            id = data.toString('utf-8').substring(3);
+
+            console.log(`Your Id is ${id}!`)
+        }else{
+            console.log();
+            await moveCursor(0,-1);
+            await clearLine(0);
+            console.log(data.toString('utf-8'))
+
+        }
+        
+        await ask();
+    })
+
 })
 
-client.on("close",() => {
-    console.log("Close")
-})
 
 client.on("end",()=>{
     console.log("Ended!")
-});
+})
--- projects/chat-app/server.js
+++ projects/chat-app/server.js
@@ -2,8 +2,33 @@ const net = require("net")
 
 const server = net.createServer()
 
+const clients = []
+
 server.on("connection", (socket)=>{
     console.log("A new connection to the server.")
+    
+    const clientId = clients.length + 1; 
+    
+    clients.map((s) => {
+        s.socket.write(`A new user ${clientId} joined!`);
+    })
+    
+    
+    socket.write(`id-${clientId.toString()}`);
+    
+    socket.on("data", (data) => {
+       clients.map((s) => {
+            s.socket.write(data);
+       })
+    })
+
+    socket.on("error", () => {
+       clients.map((s) => {
+            s.socket.write(`The client ${clientId} left!`);
+       })
+    })
+
+    clients.push({id: clientId.toString(), socket})
 });
 
 server.listen(3008,"localhost", ()=>{
